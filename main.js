fed10dc72379eea44029b443dd61ec6b97fef2e4
Josu <148201811+Catzuna@users.noreply.github.com>
2023-11-14 21:02:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 /*Condition*/
+//sir naka declare sa line 10 eon clear display 
 let display = document.getElementById('display');
 
+//Function po nung letter C para maibalik sa 0 eon value nya po
 function clearDisplay() {
     display.value = '0';
 }
 
 
 /*Function*/
+//lahat po nung addToDisplay yun po magiging value ng box
 function addToDisplay(value) {
     if (display.value === '0') {
         display.value = value;
@@ -18,8 +21,8 @@ function addToDisplay(value) {
 /*Calculate*/
 function calculate() {
     try {
-        display.value = eval(display.value);
+        display.value = eval(display.value); //pang evaluate since may function nmn po ang eval
     } catch (error) {
-        display.value = 'Error';
+        display.value = 'Error';// example ser 10 / 0.
     }
 }
